da2c7284fafb1083e5b29539f203e5dcf60c659d
treee27 <darshandineshsalunke@gmail.com>
2026-08-19 10:12:27 +0530

    learn about llm

---
+++ LLMS/llm_demo.py
@@ -0,0 +1,11 @@
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+llm = OpenAI(model = "gpt-3.5-turbo-instruct")
+
+result=llm.invoke("Write a poem about the ocean.")
+
+print(result)
+
+
